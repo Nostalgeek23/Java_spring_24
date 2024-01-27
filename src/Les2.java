0f8f95bd7b1b30a5e7a0d843473ded183a7d3f3c
Eugene <dorick23@gmail.com>
2024-01-27 21:16:09 +0300

    Lesson 2

---
+++ src/Les2.java
@@ -0,0 +1,56 @@
+public class Les2 {
+    public static void main(String[] args) {
+
+
+
+//variables
+
+        int a = 5;
+        double b = 2;
+        System.out.println(a/b);
+
+        int max = 2147483647;
+        long c = 2147483648L;
+        System.out.println(c);
+
+
+        int i = 2;
+        double d = 10.1;
+        float f = 10.1F;
+
+        long A = 1000_000_000;
+
+
+        System.out.println(a%b);
+
+
+
+//string types
+
+        String name = "Eugene";
+        String s = "!";
+        System.out.println(name + s);
+        System.out.println(a + b + s);
+
+
+//        int myApples = 3;
+//        int yourApples = 10;
+////      int hisApples = 2*myApples;
+////      int result = myApples+yourApples+hisApples;
+//        double result = (myApples+yourApples) / 2.0;
+//
+//        System.out.println(result);
+//
+//        byte one = 1;
+//        byte two = 2;
+//        int three = one+two;
+//
+//        short s = 17;
+//
+//        int r = two+s;
+//
+//        long l = 1920;
+//        long ll = 1922 + 123;
+//        System.out.println(ll);
+    }
+}
