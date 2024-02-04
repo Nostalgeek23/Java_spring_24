547e022eb6521a8353c38fa79e13ea0b49c18179
Eugene <dorick23@gmail.com>
2024-02-04 21:04:15 +0300

    HW 3_1

---
+++ src/HW_3.java
@@ -0,0 +1,15 @@
+public class HW_3 {
+    public static void main(String[] args) {
+
+        int a =42;
+        int b = 42;
+
+        if (a < b) {
+            System.out.println("a < b");
+        } else if (a > b) {
+            System.out.println("a > b");
+        } else {
+            System.out.println("a == b");
+        }
+    }
+}
