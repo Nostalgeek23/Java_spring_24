8283fbc7770dcd5601f16a486add2463df59d2b7
Eugene <dorick23@gmail.com>
2024-02-04 20:17:23 +0300

    Lesson 3: if-else engineering flowchart

---
+++ src/Les3_Engineering.java
@@ -0,0 +1,34 @@
+public class Les3_Engineering {
+    public static void main(String[] args) {
+
+        boolean itMoves = true;
+        boolean shouldItMove = true;
+
+        if (itMoves) {
+            if (shouldItMove) {
+                System.out.println("It moves, No Problem");
+            } else {
+                System.out.println("Take a tape!");
+            }
+        } else {
+           if (shouldItMove) {
+                System.out.println("take wd-40!");
+           } else {
+               System.out.println("Isn't move, No problem!");
+           }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
