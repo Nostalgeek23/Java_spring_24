5a55467ea07af0789bc724766342435f58914c42
Eugene <dorick23@gmail.com>
2024-02-05 21:56:35 +0300

    Lesson 4 for loop

---
+++ src/Les4.java
@@ -0,0 +1,38 @@
+public class Les4 {
+    public static void main(String[] args) {
+
+//стандартный цикл
+
+        for ( int i = 0; i < 5; i = i + 1 ) {
+            System.out.println(i);
+        }
+
+//цикл while
+
+        int a = 0;
+
+        while (a < 5) {
+            System.out.println(a);
+
+            a = a + 1;
+        }
+
+
+//цикл do while
+
+        int b = 0;
+
+        do {
+            System.out.println(b);
+            b = b + 1;
+        } while (b < 5);
+
+
+
+
+
+
+
+
+    }
+}
